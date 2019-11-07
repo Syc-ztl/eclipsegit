c00a5bafc08055021acecb002720fa32f1ff150c
xietingfei <2218974645@qq.com>
2019-11-07 09:43:34 +0800

    题库控制层(InformationController)的提交

---
+++ src/main/java/com/sun/chenglixin/controller/InformationController.java
@@ -0,0 +1,34 @@
+package com.sun.chenglixin.controller;
+
+import com.sun.chenglixin.entity.Information;
+import com.sun.chenglixin.service.IInformationService;
+import com.sun.chenglixin.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Description 题库控制层
+ * @Date 2019/11/5 9:43
+ * @Author wlb
+ */
+@RestController
+@RequestMapping(value = "Information")
+public class InformationController extends Exception {
+
+    @Autowired
+    IInformationService iInformationService;
+
+
+    @RequestMapping(value = "saveInformation")
+    public JsonResult<Void> saveInformation(Information information, String title, HttpSession session, String answer, String rAnswer){
+        String username = (String) session.getAttribute("username");
+        iInformationService.saveInformation(information, title, username, answer, rAnswer);
+        JsonResult<Void> json=new JsonResult<Void>();
+        json.setState(20);
+        return json;
+    }
+
+}
